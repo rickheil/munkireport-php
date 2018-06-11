46dfa4f73559e9f7169b365e46b5d254fce7c791
Rick Heil <rickheil@partnersandsimons.com>
2018-06-11 13:35:32 -0400

    update model, it now works

--- app/modules/sophos/sophos_model.php
+++ app/modules/sophos/sophos_model.php
@@ -16,23 +16,11 @@ class Sophos_model extends \Model
         $this->rs['virus_data_version'] = '';
         $this->rs['user_interface_version'] = '';
 
-        // Add indexes
-        $this->idx[] = array('serial_number');
-        $this->idx[] = array('installed');
-        $this->idx[] = array('running');
-        $this->idx[] = array('product_version');
-        $this->idx[] = array('engine_version');
-        $this->idx[] = array('virus_data_version');
-        $this->idx[] = array('user_interface_version');
-
-        // Schema version, incrememnt when creating a db migration
-        $this->schema_version = 0;
-
         if ($serial) {
             $this->retrieve_record($serial);
         }
 
-        $this->serial = $serial;
+        $this->serial_number = $serial;
     }
 
     // ------------------------------------------------------------------------
@@ -45,43 +33,17 @@ class Sophos_model extends \Model
      **/
     public function process($data)
     {
-		$parser = new CFPropertyList();
-		$parser->parse($data);
+        $parser = new CFPropertyList();
+        $parser->parse($data);
+        $plist = $parser->toArray();
 
-		$plist = $parser->toArray();
+        $this->installed = $plist['Installed'];
+        $this->running = $plist['Running'];
+        $this->engine_version = $plist['Versions']['Engine version'];
+        $this->product_version = $plist['Versions']['Product version'];
+        $this->user_interface_version = $plist['Versions']['User interface version'];
+        $this->virus_data_version = $plist['Versions']['Virus data version'];
+        $this->save();
 
-        $map = array(
-            'Engine version' => 'engine_version',
-            'Product version' => 'product_version',
-            'User interface version' => 'user_interface_version',
-            'Virus data version' => 'virus_data_version',
-        );
-
-        foreach ($map as $search => $item) {
-            if (isset($plist[$search])) {
-                if ($plist[$search] === true) {
-                    $this->$item = 1;
-                } elseif ($plist[$search] === false) {
-                    $this->$item = 0;
-                } else {
-                    $this->$item = $plist[$search];
-                }
-            } else {
-                $this->$item = '';
-            }
-        }
-
-        $this->$id = '';
-        $this->$save();
-
-#		foreach (array('running', array('versions')) as $item) {
-#			if (isset($plist[$item])) {
-#				$this->$item = $plist[$item];
-#			} else {
-#				$this->$item = '';
-#			}
-#
-#    }
-#		$this->save();
     }
 }
